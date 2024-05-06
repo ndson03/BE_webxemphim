4098339a2addbbfcb3ab07b1ef4a1dd3a77959fe
Veeci <doanvietquang15102003@gmail.com>
2024-05-06 09:53:22 +0700

    added actos list page

---
+++ Controllers/actorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NetflixClone.Models;
+
+namespace NetflixClone.Controllers
+{
+    public class actorsController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: actors
+        public ActionResult Index()
+        {
+            return View(db.actors.ToList());
+        }
+
+        // GET: actors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            actor actor = db.actors.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actor);
+        }
+
+        // GET: actors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: actors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "actorID,actorName,birthday,gender,placeOfBirth,biography,knownForDepartment,popularity,profilePath,imdbID,originalName")] actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.actors.Add(actor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(actor);
+        }
+
+        // GET: actors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            actor actor = db.actors.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actor);
+        }
+
+        // POST: actors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "actorID,actorName,birthday,gender,placeOfBirth,biography,knownForDepartment,popularity,profilePath,imdbID,originalName")] actor actor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(actor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(actor);
+        }
+
+        // GET: actors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            actor actor = db.actors.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actor);
+        }
+
+        // POST: actors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            actor actor = db.actors.Find(id);
+            db.actors.Remove(actor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
